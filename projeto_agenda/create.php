23b171b2cf82d618be1de4c421ce25091a2baf79
nicolassgross <nicolassgross1@gmail.com>
2021-10-26 17:50:20 -0300

    Finalizando o dia 26/10 com implementações no cadastro de contatos da agenda!

--- projeto_agenda/create.php
+++ projeto_agenda/create.php
@@ -1,8 +1,26 @@
 <?php
-include_once("templates/header.php")
+    include_once("templates/header.php")
 ?>
-    <h1>Criando Contato</h1>
-    <i class="fas fa-eye"></i>
+    <div class="container">
+        <?php include_once("templates/backbtn.html"); ?>
+        <h1>Criar Contato</h1>
+        <form action="<?= $BASE_URL ?>config/process.php" method="POST">
+            <input type="hidden" name="type" value="create">
+            <div class="form-group">
+                <label for="name">Nome do contato</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome aqui." required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Telefone do contato</label>
+                <input type="text" class="form-control" id="phone" phone="phone" placeholder="Digite o telefone aqui." required>
+            </div>
+            <div class="form-group">
+                <label for="details">Detalhes do contato</label>
+                <textarea type="text" class="form-control" id="details" details="details" placeholder="Digite os detalhes aqui." rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </form>
+    </div>
 <?php
     include_once("templates/footer.php")
 ?>
--- projeto_agenda/show.php
+++ projeto_agenda/show.php
@@ -2,6 +2,7 @@
     include_once("templates/header.php")
 ?>
     <div class="container" id="view-contact-container">
+        <?php include_once("templates/backbtn.html"); ?>
         <h1 id="main-title"><?= $contacts['name'] ?></h1>
         <p class="bold">Telefone:</p>
         <p id="main-title"><?= $contacts['phone'] ?></p>
