b722bb436b664139b8910400f981ec89ab939dc7
nicolassgross <nicolassgross1@gmail.com>
2021-10-26 18:01:06 -0300

    Alterações no css!

--- projeto_agenda/create.php
+++ projeto_agenda/create.php
@@ -3,8 +3,8 @@
 ?>
     <div class="container">
         <?php include_once("templates/backbtn.html"); ?>
-        <h1>Criar Contato</h1>
-        <form action="<?= $BASE_URL ?>config/process.php" method="POST">
+        <h1 id="main-title">Criar Contato</h1>
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
             <input type="hidden" name="type" value="create">
             <div class="form-group">
                 <label for="name">Nome do contato</label>
